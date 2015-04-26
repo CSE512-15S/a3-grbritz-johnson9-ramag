1bc8411dd590259506bcb895815cf859b1daa4b7
Graeme Britz <grbritz@gmail.com>
2015-04-26 15:47:38 -0700

    trying to dup bostock's county maps dataset

--- public/js/us-counties.js
+++ public/js/us-counties.js
@@ -3,12 +3,21 @@
   var map_height = 500;
 
   $(document).ready(function() {
+    var svg = d3.select('#map')
+      .append('svg')
+      .attr({
+        'width' : map_width,
+        'height' : map_height
+      });
 
-
-
-
-
-    d3.select('#map').append('svg')
+    d3.json('/datasets/census-maps/county.json', function(err, topology) {
+      console.log(topology);
+      svg.selectAll('path')
+         .data(topojson.feature(topology, topology.objects.counties).features)
+         .enter()
+         .append('path')
+         .attr('d', path);
+    });
   });
 })();
 
