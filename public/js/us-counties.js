3f109ddbe5081ae1abdf1d8a5af0a4a441dfaece
Graeme Britz <grbritz@gmail.com>
2015-04-26 18:30:35 -0700

    remove informational code from counties script

--- public/js/us-counties.js
+++ public/js/us-counties.js
@@ -11,39 +11,13 @@
       });
     var path = d3.geo.path();
 
-    // d3.json('/datasets/census-maps/us.json', function(err, topology) {
+    d3.json('/datasets/census-maps/us.json', function(err, topology) {
 
-    //   svg.selectAll('path')
-    //      .data(topojson.feature(topology, topology.objects.counties).features)
-    //      .enter()
-    //      .append('path')
-    //      .attr('d', path);
-    // });
-
-    d3.json('/datasets/census-maps/reference/county-codes.json', function(err, data) {
-      console.log("Num County Codes:", Object.keys(data).length);
-    });
-
-    d3.json('/datasets/census-maps/reference/zip-codes.json', function(err, zipCodes) {
-      var counties = {};
-
-      _.each(zipCodes, function(stateAndCounty, zipCode) {
-        // console.log("stateAndCounty", stateAndCounty);
-        _.each(stateAndCounty.countyId, function(countyId) {
-          // console.log("countyId", countyId);
-          if (! counties.hasOwnProperty(countyId)) {
-            counties[countyId] = true;
-          }
-        });
-      });
-
-      console.log("Num Counties in Zip-codes: ", Object.keys(counties).length);
-    });
-
-
-    d3.json('/datasets/census-maps/reference/zips-by_county.json', function(err, data) {
-      console.log("Num County Codes (zips-by_county):", Object.keys(data).length);
-
+      svg.selectAll('path')
+         .data(topojson.feature(topology, topology.objects.counties).features)
+         .enter()
+         .append('path')
+         .attr('d', path);
     });
   });
 })();
