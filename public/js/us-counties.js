29dc17bb2578d08c19b87d5ec90895633dc7fade
Graeme Britz <grbritz@gmail.com>
2015-04-27 11:20:19 -0700

    hover and shows county name

--- public/js/us-counties.js
+++ public/js/us-counties.js
@@ -1,52 +1,8 @@
-function SearchBox (zipCodesToCounties) {
-  var self = this;
-  self.zipCodesToCounties = zipCodesToCounties;
-
-  $(".controls-wrapper").removeClass('hide');
-  $(".controls-wrapper input[name=zip-code-filter]").on('input', function() {
-    d3.selectAll('.county').classed('selected', false);
-    var $this = $(this);
-    var zipCode = $this.val();
-    // Lets not have empty string show map as blue
-    if (! zipCode) return;
-
-    var matchingZips = self.zipCodesMatchingSubstring(zipCode, Object.keys(self.zipCodesToCounties));
-
-    var countyDivs = self.countyDivsMatchingZips(matchingZips, self.zipCodesToCounties);
-    d3.selectAll(countyDivs).classed('selected', true);
-  });
-
-  this.countyDivsMatchingZips = function(zipCodes, zipCodesToCounties) {
-    var counties = {};
-    _.each(zipCodes, function(zipCode) {
-      _.each(zipCodesToCounties[zipCode], function(county) {
-        // Convert countyid into classname
-        county = "#cid-" + county;
-        if (! counties.hasOwnProperty(county)) {
-          counties[county] = true;
-        }
-      });
-    });
-
-    counties = Object.keys(counties);
-    return $(counties.join(", ")).get();
-  }
-
-  this.zipCodesMatchingSubstring = function(zipSubStr, zipCodes) {
-    return _.filter(zipCodes, function(zipCode) {
-      zipCode = zipCode + ""; // Massage into string just in case
-      return zipCode.indexOf(zipSubStr) === 0;
-    });
-  }  
-}
-
-
 (function () {
   var map_width = 1024;
   var map_height = 500;
 
-  var g_zipCodesToCounties;
-
+  var datasetCache = {};
 
   function countyId (datum) {
     var id = datum.id + "";
@@ -61,6 +17,31 @@ function SearchBox (zipCodesToCounties) {
     return null;
   }
 
+  function toggleCountyTooltip (countyId, showToolTip) {
+    var referenceId = countyId.split('-')[1];
+    // For now, fail silently when data is not loaded
+    if (! datasetCache['countyDetails']) return;
+    
+
+    var boundingBox = d3.select('#' + countyId).node().getBBox();
+    tooltipPosition = {
+      'left' : (boundingBox.x + (boundingBox.width / 2)) + "px",
+      'top' : (boundingBox.y + (boundingBox.height / 2)) + "px"
+    }
+
+    var details = datasetCache['countyDetails'][referenceId];
+
+    if (showToolTip) {
+      $(".county-tooltip").css(tooltipPosition)
+                          .text(details.name)
+                          .css('visibility', 'visible');
+    }
+    else {
+      $(".county-tooltip").css('visibility', 'hidden')
+                          .text("");
+    }
+  }
+
   // "Main" fn; starts after all DOM elements have loaded
   $(document).ready(function() {
     var svg = d3.select('#map')
@@ -71,6 +52,7 @@ function SearchBox (zipCodesToCounties) {
       });
     var path = d3.geo.path();
 
+    // Load map tiles
     d3.json('/datasets/census-maps/us.json', function(err, topology) {
 
       svg.selectAll('path')
@@ -85,27 +67,72 @@ function SearchBox (zipCodesToCounties) {
          .classed('county', true)
          .on('mouseover', function(datum, index) {
             d3.select(this).classed('hover', true);
-            // displayCountyTooltip();
+            toggleCountyTooltip(this.id, true);
          })
          .on('mouseout', function(datum, index) {
             d3.select(this).classed('hover', false);
+            toggleCountyTooltip(this.id, false);
          })
          .on('click', countyClickHandler);
     });
+
+    // Load county reference details
+    d3.json('/datasets/census-maps/reference/county-codes.json', function(err, countyDetails) {
+      datasetCache['countyDetails'] = countyDetails;
+    });
+  
+    // Load zip code to county reference details & activate search box
+    d3.json('/datasets/census-maps/reference/zip-codes-to-counties.json', 
+      function(err, zipCodesToCounties) {
+      var searchBox = new SearchBox(zipCodesToCounties);
+      
+      datasetCache['zipCodesToCounties'] = zipCodesToCounties;
+    });
+
   });
 
-  
 
-  d3.json('/datasets/census-maps/reference/zip-codes-to-counties.json', 
-    function(err, zipCodesToCounties) {
-    var searchBox = new SearchBox(zipCodesToCounties);
-    g_zipCodesToCounties = zipCodesToCounties;
-  });
+  function SearchBox (zipCodesToCounties) {
+    var self = this;
+    self.zipCodesToCounties = zipCodesToCounties;
 
+    $(".controls-wrapper").removeClass('hide');
+    $(".controls-wrapper input[name=zip-code-filter]").on('input', function() {
+      d3.selectAll('.county').classed('selected', false);
+      var $this = $(this);
+      var zipCode = $this.val();
+      // Lets not have empty string show map as blue
+      if (! zipCode) return;
 
+      var matchingZips = self.zipCodesMatchingSubstring(zipCode, Object.keys(self.zipCodesToCounties));
 
+      var countyDivs = self.countyDivsMatchingZips(matchingZips, self.zipCodesToCounties);
+      d3.selectAll(countyDivs).classed('selected', true);
+    });
 
+    this.countyDivsMatchingZips = function(zipCodes, zipCodesToCounties) {
+      var counties = {};
+      _.each(zipCodes, function(zipCode) {
+        _.each(zipCodesToCounties[zipCode], function(county) {
+          // Convert countyid into classname
+          county = "#cid-" + county;
+          if (! counties.hasOwnProperty(county)) {
+            counties[county] = true;
+          }
+        });
+      });
 
-  
+      counties = Object.keys(counties);
+      return $(counties.join(", ")).get();
+    }
+
+    this.zipCodesMatchingSubstring = function(zipSubStr, zipCodes) {
+      return _.filter(zipCodes, function(zipCode) {
+        zipCode = zipCode + ""; // Massage into string just in case
+        return zipCode.indexOf(zipSubStr) === 0;
+      });
+    }  
+  }
+
 })();
 
